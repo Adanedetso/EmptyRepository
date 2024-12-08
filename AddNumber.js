ff789a207897b8e49f5839a814079b8aac3703a8
Adane Detso <adanedetso@gmail.com>
2024-12-08 13:43:08 +0700

    first commit

---
+++ AddNumber.js
@@ -0,0 +1,9 @@
+// // Adding two nuber dynamically
+function add(a, b) {
+  if (a > b) {
+    console.log(`Hello user :${a} is greter than ${b}`);
+  } else {
+    console.log(`Hello user :${b} is greter than ${a}`);
+  }
+}
+add(5, 7);
